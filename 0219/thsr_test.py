945e4524f4c8c242d175834d3c49ff0d5af28a37
rick0955970236 <rick0955970236@gmail.com>
2025-02-20 11:19:38 +0800

    Signed-off-by: rick0955970236 <rick0955970236@gmail.com>

--- 0219/thsr_test.py
+++ 0219/thsr_test.py
@@ -1,12 +1,20 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select # 下拉式選單使用
+from selenium.webdriver.support.select import Select  # 下拉式選單使用
+from selenium.common.exceptions import NoSuchElementException # Handle exception
 from ocr_component import get_captcha_code
 
-driver = webdriver.Chrome()
+# 越過反爬蟲
+options = webdriver.ChromeOptions()  # 創立 driver物件所需的參數物件
+options.add_argument("--disable-blink-features=AutomationControlled")
+driver = webdriver.Chrome(options=options)
+
+# driver = webdriver.Chrome() # 無須越過反爬蟲的話直接從這行開始
 driver.get("https://irs.thsrc.com.tw/IMINT/")
-accept_cookie_button = driver.find_element(By.ID, "cookieAccpetBtn") # cookie
+
+# cookie
+accept_cookie_button = driver.find_element(By.ID, "cookieAccpetBtn")
 accept_cookie_button.click()
 
 # 找到下拉選單元素
@@ -28,21 +36,31 @@ date_input.click()  # 點擊，打開日期選擇器
 time.sleep(1)  # 等待日曆展開
 # 找到並點擊 "2025 年 3 月 1 日"
 star_date = '三月 1, 2025'
-date_element = driver.find_element(By.XPATH, "//span[@class='flatpickr-day' and @aria-label='{star_date}']")
+date_element = driver.find_element(By.XPATH, f"//span[@class='flatpickr-day' and @aria-label='{star_date}']")
 date_element.click()
 
-# captcha 獲取圖片
-captcha_img = driver.find_element(By.ID, 'BookingS1Form_homeCaptcha_passCode')
-captcha_img.screenshot('captcha.png')
-captcha_code = get_captcha_code() # 呼叫外部函式
-# 輸入驗證碼
-captcha_input = driver.find_element(By.ID, 'securityCode')
-captcha_input.send_keys(captcha_code)
+while True:
+    # captcha
+    captcha_img = driver.find_element(
+        By.ID, 'BookingS1Form_homeCaptcha_passCode')
+    captcha_img.screenshot('captcha.png')
+    captcha_code = get_captcha_code()
+    captcha_input = driver.find_element(By.ID, 'securityCode')
+    captcha_input.send_keys(1234)
+    time.sleep(2)
 
-time.sleep(5)
-# submit 開始查詢
-driver.find_element(By.ID, 'SubmitButton').click()
+    # submit
+    driver.find_element(By.ID, 'SubmitButton').click()
+    time.sleep(5)
 
-time.sleep(20)
+    # check validation is success or not
+    try:
+        # driver.find_element(By.CLASS_NAME, 'uk-alert-danger uk-alert')
+        driver.find_element(By.ID, 'divErrMSG')
+    except NoSuchElementException:
+        print("進到第二步驟")
+        break
+
+
+time.sleep(2000)
 driver.quit()
-
