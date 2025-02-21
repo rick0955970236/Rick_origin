15c6395443ed8a4e7ea2194a04b653a85e4c2a98
rick0955970236 <rick0955970236@gmail.com>
2025-02-21 14:56:49 +0800

    Signed-off-by: rick0955970236 <rick0955970236@gmail.com>

--- 0219/flask_sample.py
+++ 0219/flask_sample.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import escape
 from chatgpt_sample import chat_with_chatgpt
 
 app = Flask(__name__)
-
 
 
 @app.route("/")
@@ -10,17 +10,30 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/home")
-def hello_home():
+@app.route("/test/<int:user_id>/")
+def hello_user(user_id):
+    return f"<p>Hello USER-{escape(user_id)}, World!</p>"
+
+
+@app.route("/test/<path:subpath>/")
+def hello_path(subpath):
+    return f"<p>Hello PATH-{escape(subpath)}, World!</p>"
+
+
+@app.route("/test/<user_message>/")
+def hello_home(user_message):
     chatgpt_response = chat_with_chatgpt(
-        user_message="你是誰?可以跟我打招呼嗎?",
-        system_prompt="妳是一位後端管理員，有前端使用者會呼叫你"
+        user_message=user_message,
+        system_prompt="你是一位後端管理員，有前端使用者會呼叫你。"
     )
     return chatgpt_response
 
 
+@app.route("/sample/")
+def show_html_sample():
+    return render_template('sample.html')
 
 
-# 如果要使用使用 python xxx.py 執行
+# 如果要使用 python xxx.py 執行
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ workflow/flask_server.md
@@ -0,0 +1,14 @@
+1. 執行你的Flask App, 使其運行在你的本地電腦(http://127.0.0.1:5000)
+    
+    * 一般模式
+        > flask --app <你的應用程式檔案名稱, 不用.py> run
+
+    * Debug 模式
+        > flask --app <你的應用程式檔案名稱, 不用.py> run --debug 
+
+    * Python 執行: 設定會由程式碼提供
+        > python <你的應用程式檔案名稱>
+
+
+
+* CMD 按 Ctrl+c 可以關閉執行中的程序
